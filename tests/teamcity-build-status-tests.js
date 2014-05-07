a71a1c75a70ac0d4d50f42f7957f00ba6093cad0
iainjmitchell <iainjmitchell@gmail.com>
2014-05-07 15:54:05 +0100

    requesting build stages for project

--- tests/teamcity-build-status-tests.js
+++ tests/teamcity-build-status-tests.js
@@ -1,3 +1,26 @@
-test('bob', function(){
-	equal(true, true);
-});
+(function($){
+
+	test('bob', function(){
+		equal(true, true);
+	});
+
+	test('Requests build stages for project', function(done){
+		var projectId = Math.random(),
+			requests = [];
+		$.ajax = function(options){
+			requests.push(options);
+		};
+		$('#display').teamCityBuildStatus({
+			projectId : projectId
+		});
+		equal(requests[0].uri, 'http://teamcity.dev/guestAuth/app/rest/projects/id:' + projectId);
+	});
+
+	$.fn.teamCityBuildStatus = function(options){
+		return this.each(function(){
+			$.ajax({
+				uri : 'http://teamcity.dev/guestAuth/app/rest/projects/id:' + options.projectId
+			});
+		});
+	};
+})(jQuery, undefined);
